Fix eval loop to always choose action greedily, and fix gradient saving in training loop. Explicitly set train and eval mode of model. Clip gradients. Initialize model weights using Kaiming and Xavier initialization. Set epsilon decay to be slower

dqn/dqn_agent.py
@@ -7,15 +7,16 @@ class DQNAgent():
         self.num_actions = num_actions
         self.device = device
 
-    # Given a state, returns the greedy action with (1-epsilon) probability and a random action otherwise
-    def get_action(self, state, dqn):
-        epsilon = self.exploration_strategy.get_exploration_rate()
-        if random.random() <= epsilon:
-            action = self.select_random_action()
-            action = torch.tensor([action], dtype=torch.long).to(self.device)
-        else:
-            with torch.no_grad():
-                logits = dqn(state)
+    # Given a state, returns the greedy action with (1-epsilon) probability and a random action otherwise, if in training phase
+    # Else takes greedy action
+    def get_action(self, state, dqn, is_training=True):
+        if is_training:
+            epsilon = self.exploration_strategy.get_exploration_rate()
+            if random.random() <= epsilon:
+                action = self.select_random_action()
+                action = torch.tensor([action], dtype=torch.long).to(self.device)
+            else:
+                # logits = dqn(state)
                 # print(f"Feedforward through dqn to get action, shape: {logits.size()}")
                 # print(f"Logits: {logits}")
                 # action = dqn(state).argmax(dim=1).to(self.device)
@@ -23,11 +24,21 @@ class DQNAgent():
                 action = dqn(state).argmax()
                 action = torch.tensor([action]).to(self.device)
 
-        # Now that you have selected the action, decay the exploration rate so that you are more likely to select greedy action
-        self.exploration_strategy.decay_exploration_rate()
+            # Now that you have selected the action, decay the exploration rate so that you are more likely to select
+            # greedy action on next action selection
+            # print(f"**************Epsilon exploration rate: {self.exploration_strategy.get_exploration_rate()}")
+            self.exploration_strategy.decay_exploration_rate()
+        else:
+            # Not training anymore, so want to always take action with highest Q value
+            with torch.no_grad():
+                action = dqn(state).argmax()
+                action = torch.tensor([action]).to(self.device)
 
         return action
 
     def select_random_action(self):
         # For now, this just returns an integer within range of 0 to num_actions - 1 for the selected action (cartpole only has two actions)
         return random.randint(0, self.num_actions - 1)
+
+    def get_epsilon(self):
+        return self.exploration_strategy.get_exploration_rate()

dqn/env_manager.py
@@ -54,20 +54,22 @@ class EnvManager():
             # Learn on random batch from replay memory
             self.learn()
 
+        print(f"Epsilon: {self.agent.get_epsilon()}")
         print(f"Finished the episode after {num_steps} steps. Reward: {score}")
         self.done = False
 
     # CartPole-v0 defines solving the env as getting average reward of 195 over 100 consecutive trials
     def evaluate_model(self, num_eval_episodes):
+        self.dqn.eval()
         for _ in range(num_eval_episodes):
             num_steps = 0
             score = 0
             state = self.env.reset()
             state = torch.tensor(state, dtype=torch.float32, device=self.device)
-            while not self.done or num_steps < 1000:
+            while not self.done and num_steps < 1000:
                 num_steps += 1
                 # Select and take action
-                action = self.agent.get_action(state=state, dqn=self.dqn)
+                action = self.agent.get_action(state=state, dqn=self.dqn, is_training=False)
                 next_state, reward = self.take_action(action.item())
                 score += reward.item()
                 state = next_state
@@ -81,6 +83,7 @@ class EnvManager():
 
     def learn(self):
         try:
+            self.dqn.train()
             replay_states, replay_actions, replay_rewards, replay_next_states = self.memory.sample(self.replay_batch_size)
             current_q_values = self.dqn.get_current_q_values(replay_states, replay_actions)
             next_q_values = self.dqn.get_next_q_values(replay_next_states)
@@ -90,6 +93,9 @@ class EnvManager():
             loss = F.mse_loss(current_q_values, target_q_values.unsqueeze(1))
             self.optimizer.zero_grad()
             loss.backward()
+            # Clip gradients
+            for param in self.dqn.parameters():
+                param.grad.data.clamp_(-1, 1)
             self.optimizer.step()
         except Exception as e:
             # This could occur if not enough experiences in replay buffer yet